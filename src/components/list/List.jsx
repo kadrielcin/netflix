3064d43b3db0c43d813ae66a799f9c4468613df6
maskelisuvari1211 <kadrielcin@yahoo.com>
2022-07-19 12:45:46 +0200

    listItem

--- src/components/list/List.jsx
+++ src/components/list/List.jsx
@@ -1,9 +1,27 @@
+import {
+  ArrowBackIosOutlined,
+  ArrowForwardIosOutlined,
+} from "@material-ui/icons";
+import ListItem from "../listitem/ListItem";
 import "./list.css";
 
 export default function List() {
   return (
     <div className="list">
-      <span className="listTitle"></span>
+      <span className="listTitle">Continue to watch</span>
+      <div className="wrapper">
+        <ArrowBackIosOutlined />
+        <div className="container">
+          <ListItem />
+          <ListItem />
+          <ListItem />
+          <ListItem />
+          <ListItem />
+          <ListItem />
+          <ListItem />
+        </div>
+        <ArrowForwardIosOutlined />
+      </div>
     </div>
   );
 }
---
+++ src/components/listItem/ListItem.jsx
@@ -0,0 +1,5 @@
+import React from "./listItem.css";
+
+export default function ListItem() {
+  return <div className="listItem"></div>;
+}
